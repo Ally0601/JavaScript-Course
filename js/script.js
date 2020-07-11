e1fe58ba39d18296b5b3ed9ae608aee6501cbe97
Almash <almash.alam.01@gmail.com>
2020-07-11 23:59:32 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,103 @@
+
+
+var slideIndex = 0;
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, document.getElementById("timeinterval").value*1000); // Change image every 2 seconds
+}
+
+// =========================================================================================================================== 
+// =========================================================================================================================== 
+// =========================================================================================================================== 
+// =========================================================================================================================== 
+
+
+// select all the buttons
+const buttons = document.querySelectorAll('button');
+// select the <input type="text" class="display" disabled> element
+const display = document.querySelector('.display');
+
+// add eventListener to each button
+buttons.forEach(function(button) {
+  button.addEventListener('click', calculate);
+});
+
+// calculate function
+function calculate(event) {
+  // current clicked buttons value
+  const clickedButtonValue = event.target.value;
+
+  if (clickedButtonValue === '=') {
+    // check if the display is not empty then only do the calculation
+    if (display.value !== '') {
+      // calculate and show the answer to display
+      display.value = eval(display.value);
+    }
+  } else if (clickedButtonValue === 'C') {
+    // clear everything on display
+    display.value = '';
+  } else {
+    // otherwise concatenate it to the display
+    display.value += clickedButtonValue;
+  }
+}
+
+// =========================================================================================================================== 
+// =========================================================================================================================== 
+// =========================================================================================================================== 
+// =========================================================================================================================== 
+
+const form = document.getElementById('id');
+var email = document.getElementById('email');
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+}
+
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function emailVerification() {
+  checkRequired([email]);
+  checkEmail(email);
+}
+
+
+
